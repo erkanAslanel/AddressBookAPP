c3e06a3c14a5c7966da3307011f27e9a95d8923a
Erkan Aslanel <erkan.aslanel@gmail.com>
2021-05-31 22:24:20 +0300

    initProject

---
+++ AddressBookAPI/Controllers/ContactController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AddressBookAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactController : ControllerBase
+    {
+        private readonly ILogger<ContactController> _logger;
+
+        public ContactController(ILogger<ContactController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create(ContactCreateCommand command)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            return Ok();
+        }
+    }
+}
---
+++ AddressBookDTO/Command/ContactCreateCommand.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class ContactCreateCommand{
+
+public string Name { get; set; }
+public string LastName { get; set; }
+
+public string Firm { get; set; }
+
+}
